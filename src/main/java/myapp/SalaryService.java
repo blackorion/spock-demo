8fbe7e599e4806098a753e110e36b2e5b1c68d3a
orion <crissorion@gmail.com>
2016-09-27 18:21:42 +0300

    basic blocks

--- src/main/java/myapp/SalaryService.java
+++ src/main/java/myapp/SalaryService.java
@@ -2,5 +2,26 @@ package myapp;
 
 class SalaryService {
 
+    public static final int HOUR_SALARY = 10;
+
+    public int calculate(int workingHours) {
+        return salaryFor(standardHoursFrom(workingHours))
+                + salaryFor(overtimeHoursFrom(workingHours));
+    }
+
+    private int salaryFor(int hours) {
+        return hours * HOUR_SALARY;
+    }
+
+    private int standardHoursFrom(int hours) {
+        return hours > 8 ? 8 : hours;
+    }
+
+    private int overtimeHoursFrom(int hours) {
+        int overtime = hours > 8 ? hours - 8 : 0;
+
+        return (int) (overtime * 1.5);
+    }
+
 }
 
--- src/test/groovy/myapp/SalaryServiceSpec.groovy
+++ src/test/groovy/myapp/SalaryServiceSpec.groovy
@@ -5,9 +5,27 @@ import spock.lang.Specification
 class SalaryServiceSpec extends Specification {
     SalaryService service = new SalaryService()
 
-    def "should initiate application field"() {
-        expect:
-        service
+    def "should return zero when no working hours"() {
+        when:
+        def salary = service.calculate(0)
+
+        then:
+        salary == 0
     }
 
+    def "should return 80 when worked 8 hours"() {
+        when:
+        def salary = service.calculate(8)
+
+        then:
+        salary == 80
+    }
+
+    def "should return 110 when worked 10 hours"() {
+        when:
+        def salary = service.calculate(10)
+
+        then:
+        salary == 110
+    }
 }
